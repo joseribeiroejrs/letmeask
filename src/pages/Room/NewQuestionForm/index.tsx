0e35a3a7e3b15f6d7cc6b9e63d33f3ebff2c87aa
Eduardo Soares <eduardo.soares@zup.com.br>
2021-06-26 16:52:04 -0300

    Refatorando Room, separando em funcionalidade de Like e Formulario

---
+++ src/pages/Room/NewQuestionForm/index.tsx
@@ -0,0 +1,75 @@
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Button } from "../../../components/Button";
+import { UserType } from "../../../contexts/AuthContext";
+import { useAuth } from "../../../hooks/useAuth";
+import { database } from "../../../services/firebase";
+import { RoomParams } from "../../AdminRoom/AdminQuestionsOptions";
+
+// TODO: REACT HOT TOAST COLOCAR NO FUTURO
+// TODO: REACT MODAL TAMBEM
+export const NewQuestionForm = (): JSX.Element => {
+	const { user } = useAuth();
+	const params = useParams<RoomParams>();
+	const roomId = params.id;
+	const [newQuestion, setNewQuestion] = useState("");
+
+	const handleNewQuestionChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+		setNewQuestion(event.target.value);
+	};
+
+	const getRequestPayload = (content: string, user: UserType) => {
+		const { name, avatar } = user;
+
+		return {
+			content,
+			author: {
+				name,
+				avatar,
+			},
+			isHighlighted: false,
+			isAnswered: false,
+		};
+	};
+
+	const handleSendQuestion = async (event: FormEvent) => {
+		event.preventDefault();
+
+		if (newQuestion.trim() === "") {
+			return;
+		}
+
+		if (!user) {
+			throw new Error("You must be logged in ");
+		}
+
+		const question = getRequestPayload(newQuestion, user);
+		await database.ref(`rooms/${roomId}/questions`).push(question);
+		setNewQuestion("");
+	};
+
+	return (
+		<form onSubmit={(event) => handleSendQuestion(event)}>
+			<textarea
+				placeholder="O que você quer perguntar?"
+				onChange={handleNewQuestionChange}
+			></textarea>
+			<div className="form-footer">
+				{user ? (
+					<div className="user-info">
+						<img src={user?.avatar} alt={user?.name} />
+						<span>{user?.name}</span>
+					</div>
+				) : (
+					<span>
+						Para enviar uma pergunta, <button>faça seu login</button>.
+					</span>
+				)}
+
+				<Button type="submit" disabled={!user}>
+					Enviar pergunta
+				</Button>
+			</div>
+		</form>
+	);
+};
--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,108 +1,36 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import LogoImg from "../../assets/images/logo.svg";
-import { Button } from "../../components/Button";
+import React from "react";
+import { useParams } from "react-router-dom";
 import { Question } from "../../components/Question";
-import { useAuth } from "../../hooks/useAuth";
 import { useRoom } from "../../hooks/useRoom";
-import { database } from "../../services/firebase";
-import { RoomCode } from "../../components/RoomCode";
-import "../../styles/room.scss";
 import { Empty, EmptyType } from "../../components/Empty";
 import { LayoutRoom } from "../../components/LayoutRoom";
+
+import { Like } from "./Like";
+import { NewQuestionForm } from "./NewQuestionForm";
+
+import "../../styles/room.scss";
 
 type RoomParams = {
 	id: string;
 };
 
 export const Room = (): JSX.Element => {
-	const { user } = useAuth();
-	const history = useHistory();
 	const params = useParams<RoomParams>();
 	const roomId = params.id;
-	const { title, questions } = useRoom(roomId);
+	const { questions } = useRoom(roomId);
 
-	const [newQuestion, setNewQuestion] = useState("");
-	const emptyLabels: EmptyType = {
-		title: "Nenhuma pergunta por aqui...",
-		description:
-			"Faça o seu login e seja a primeira pessoa a fazer uma pergunta!",
-	};
-
-	const handleNewQuestionChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-		setNewQuestion(event.target.value);
-	};
-
-	const goToHome = () => {
-		history.push("/");
-	};
-
-	const handleSendQuestion = async (event: FormEvent) => {
-		event.preventDefault();
-		if (newQuestion.trim() === "") {
-			return;
-		}
-		if (!user) {
-			// TODO: REACT HOT TOAST COLOCAR NO FUTURO
-			// TODO: REACT MODAL TAMBEM
-			throw new Error("You must be logged in ");
-		}
-
-		const { name, avatar } = user;
-
-		const question = {
-			content: newQuestion,
-			author: {
-				name,
-				avatar,
-			},
-			isHighlighted: false,
-			isAnswered: false,
+	const renderEmptyQuestionList = () => {
+		const emptyLabels: EmptyType = {
+			title: "Nenhuma pergunta por aqui...",
+			description:
+				"Faça o seu login e seja a primeira pessoa a fazer uma pergunta!",
 		};
-
-		await database.ref(`rooms/${roomId}/questions`).push(question);
-
-		setNewQuestion("");
-	};
-
-	const handleLikeQuestion = async (
-		questionId: string,
-		likeId: string | undefined
-	) => {
-		const query = `rooms/${roomId}/questions/${questionId}/likes`;
-		if (likeId) {
-			await database.ref(`${query}/${likeId}`).remove();
-		} else {
-			await database.ref(query).push({
-				authorId: user?.id,
-			});
-		}
+		return !questions?.length && <Empty {...emptyLabels} />;
 	};
 
 	return (
 		<LayoutRoom>
-			<form onSubmit={(event) => handleSendQuestion(event)}>
-				<textarea
-					placeholder="O que você quer perguntar?"
-					onChange={handleNewQuestionChange}
-				></textarea>
-				<div className="form-footer">
-					{user ? (
-						<div className="user-info">
-							<img src={user?.avatar} alt={user?.name} />
-							<span>{user?.name}</span>
-						</div>
-					) : (
-						<span>
-							Para enviar uma pergunta, <button>faça seu login</button>.
-						</span>
-					)}
-
-					<Button type="submit" disabled={!user}>
-						Enviar pergunta
-					</Button>
-				</div>
-			</form>
+			<NewQuestionForm></NewQuestionForm>
 
 			<div className="question-list">
 				{questions.map((question) => (
@@ -113,37 +41,11 @@ export const Room = (): JSX.Element => {
 						isAnswered={question.isAnswered}
 						isHighlighted={question.isHighlighted}
 					>
-						{!question.isAnswered && (
-							<button
-								className={`like-button ${question.likeId ? "liked" : ""}`}
-								type="button"
-								aria-label="Marcar como gostei"
-								onClick={() =>
-									handleLikeQuestion(question.id as string, question?.likeId)
-								}
-							>
-								{question.likeCount > 0 && <span>{question.likeCount}</span>}
-								<svg
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z"
-										stroke="#737380"
-										strokeWidth="1.5"
-										strokeLinecap="round"
-										strokeLinejoin="round"
-									/>
-								</svg>
-							</button>
-						)}
+						<Like question={question} />
 					</Question>
 				))}
 
-				{!questions?.length && <Empty {...emptyLabels} />}
+				{renderEmptyQuestionList()}
 			</div>
 		</LayoutRoom>
 	);
