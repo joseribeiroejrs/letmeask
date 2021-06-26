7291407a1d0e7909ab1cedb102f723f96048c9fd
Eduardo Soares <eduardo.soares@zup.com.br>
2021-06-26 11:56:13 -0300

    Configurando eslint e editor config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { ReactNode } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
@@ -5,19 +6,19 @@ import { AuthContextProvider } from "./contexts/AuthContext";
 import { Room } from "./pages/Room";
 import { AdminRoom } from "./pages/AdminRoom";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <AuthContextProvider>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/rooms/new" component={NewRoom} />
-          <Route path="/rooms/:id" component={Room} />
-          <Route path="/admin/rooms/:id" component={AdminRoom} />
-        </Switch>
-      </AuthContextProvider>
-    </BrowserRouter>
-  );
-}
+const App = (): ReactNode => {
+	return (
+		<BrowserRouter>
+			<AuthContextProvider>
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/rooms/new" component={NewRoom} />
+					<Route path="/rooms/:id" component={Room} />
+					<Route path="/admin/rooms/:id" component={AdminRoom} />
+				</Switch>
+			</AuthContextProvider>
+		</BrowserRouter>
+	);
+};
 
 export default App;
